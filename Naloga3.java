1fa306e972c239792e3f1cae007804b71ac5df0d
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-06 18:08:09 +0200

    Add files via upload

---
+++ Naloga3.java
@@ -0,0 +1,29 @@
+package Naloge;
+import java.util.Scanner;
+
+public class Naloga3 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Vnesi dolzine treh stranic trikotnika: ");
+		
+		Scanner vhod = new Scanner (System.in);
+		int a = vhod.nextInt();
+		int b = vhod.nextInt();
+		int c = vhod.nextInt();
+		
+		if ((a + b > c) && (a + c > b) && (b + c > a)){
+		
+				double s = 1.0/2*(a + b + c);
+				double p = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+				System.out.println("Trikotnik obstaja, ploscina je " + p);}
+		
+		else {
+			if ((a + b == c) || (a + c == b) || (b + c == a))
+				System.out.println("Trikotnik je izrojen.");
+			
+			else
+				System.out.println("Trikotnik ne obstaja.");}
+	}
+
+}
