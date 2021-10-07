0009c339d664c78b4a5e1cb97618ae16d0564840
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-07 20:57:51 +0200

    Add files via upload

---
+++ Naloga5.java
@@ -0,0 +1,31 @@
+package Naloge;
+import java.util.Scanner;
+
+public class Naloga5 {
+
+	public static void main(String[] args) {
+		
+		Scanner vnos = new Scanner(System.in);
+		
+		System.out.println("Vnesi stevilko meseca v letu: ");
+		int mesec = vnos.nextInt();
+		System.out.println("Vnesi leto: ");
+		int leto = vnos.nextInt();
+		int dni = 0;
+		
+		switch (mesec) {
+			case 1: case 3: case 5: case 7: case 8: case 10: case 12: dni = 31;break;
+			case 4: case 6: case 9: case 11: dni = 30; break;
+			case 2:
+				if (((leto % 4 == 0) && !(leto % 100 == 0)) || (leto % 400 == 0)) {
+					dni = 29;
+				}
+				else {
+					dni = 28;
+				};
+				System.out.println("Mesec ima " + dni + " dni.");break;
+			default: System.out.println("Ni mesec.");
+		}
+	}
+
+}
