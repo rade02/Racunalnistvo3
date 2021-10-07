c8c7280c3694b24a1b8ed300b2a82e2664658239
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-07 19:28:00 +0200

    Add files via upload

---
+++ Naloga1.java
@@ -0,0 +1,21 @@
+package Naloge;
+import java.util.Scanner;
+
+public class Naloga1 {
+	public static void main(String[] args) {
+		
+		Scanner vnos = new Scanner(System.in);
+		
+		System.out.println("Vnesi diagonalo TV v incih: ");
+		int inch = vnos.nextInt();
+		
+		double d = inch * 2.54;
+		
+		int m = (int)(d / 100);
+		int dm = (int)((d % 100)/10);
+		int cm = (int)(d % 10);
+		int mm = (int)(Math.round((d % 1) * 10)); //zaokrozimo
+		
+		System.out.println(m + "m " + dm + "dm " + cm + "cm " + mm + "mm");
+	}
+}
