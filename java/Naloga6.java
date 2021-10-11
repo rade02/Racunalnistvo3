81f121033b117018f8e650ec22e0bd4345cd6ca6
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-11 20:54:32 +0200

    Add files via upload

---
+++ java/Naloga6.java
@@ -0,0 +1,26 @@
+package Naloge;
+import java.util.Scanner;
+
+public class Naloga6 {
+
+	public static void main(String[] args) {
+		
+		Scanner vnos = new Scanner(System.in);
+		
+		System.out.println("Vnesi crko kategorije vozila: ");
+		char K = vnos.nextLine().charAt(0);
+		
+		switch (K) {
+		case 'A': System.out.println("motorna kolesa");break;
+		case 'B': System.out.println("motorna vozila z NDM < 3500 kg");break;
+		case 'C': System.out.println("motorna vozila z NDM > 3500 kg");break;
+		case 'D': System.out.println("motorna vozila za prevoz oseb");break;
+		case 'E': System.out.println("motorna vozila s priklopnim vozilom");break;
+		case 'F': System.out.println("traktorji in traktorski priklopniki");break;
+		case 'G': System.out.println("delovni stroji in motokultivatorji");break;
+		default: System.out.println("Napacen vnos kategorije vozila.");break;
+		}
+		
+	}
+
+}
