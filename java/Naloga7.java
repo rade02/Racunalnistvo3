2fd025aad16e41fb1e4713f271a9c2f9de8d4aff
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-11 20:54:48 +0200

    Add files via upload

---
+++ java/Naloga7.java
@@ -0,0 +1,25 @@
+package Naloge;
+import java.util.Scanner;
+
+public class Naloga7 {
+
+	public static void main(String[] args) {
+
+		Scanner vnos = new Scanner(System.in);
+		
+		System.out.println("Vnesi kategorijo vozila za cestninjenje: ");
+		String K = vnos.nextLine();
+		int cena = 0;
+		
+		switch(K) {
+		case "C1": case "D": case "C1E": cena = 14; 
+		System.out.println("Vrednost cestnine je " + cena + ".");break;
+		case "C": case "CE": case "DE": cena = 19;
+		System.out.println("Vrednost cestnine je " + cena + ".");break;
+		case "A": case "B":
+		System.out.println("Vrednost cestnine je " + cena + ".");break;
+		default:System.out.println("Napacen vnos kategorije.");break;
+		}
+	}
+
+}
