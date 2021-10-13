ffdb8f5dccf273de01577d1f869a9337c801bd16
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-13 20:14:19 +0200

    Update Naloga7.java

--- java/RAC/Naloga7.java
+++ java/RAC/Naloga7.java
@@ -1,4 +1,3 @@
-package Naloge;
 import java.util.Scanner;
 
 public class Naloga7 {
@@ -9,15 +8,16 @@ public class Naloga7 {
 		
 		System.out.println("Vnesi kategorijo vozila za cestninjenje: ");
 		String K = vnos.nextLine();
+		vnos.close();
 		int cena = 0;
 		
 		switch(K) {
 		case "C1": case "D": case "C1E": cena = 14; 
-		System.out.println("Vrednost cestnine je " + cena + ".");break;
+		System.out.println("Vrednost cestnine je " + cena + " €.");break;
 		case "C": case "CE": case "DE": cena = 19;
-		System.out.println("Vrednost cestnine je " + cena + ".");break;
+		System.out.println("Vrednost cestnine je " + cena + " €.");break;
 		case "A": case "B":
-		System.out.println("Vrednost cestnine je " + cena + ".");break;
+		System.out.println("Vrednost cestnine je " + cena + " €.");break;
 		default:System.out.println("Napacen vnos kategorije.");break;
 		}
 	}
