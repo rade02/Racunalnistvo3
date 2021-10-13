39c701d6983a11dc5da615e9c218e8fb4015e518
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-13 20:11:04 +0200

    Update Naloga3.java

--- java/RAC/Naloga3.java
+++ java/RAC/Naloga3.java
@@ -10,12 +10,13 @@ public class Naloga3 {
 		int a = vhod.nextInt();
 		int b = vhod.nextInt();
 		int c = vhod.nextInt();
+		vhod.close();
 		
 		if ((a + b > c) && (a + c > b) && (b + c > a)){
 		
 				double s = 1.0/2*(a + b + c);
 				double p = Math.sqrt(s*(s-a)*(s-b)*(s-c));
-				System.out.println("Trikotnik obstaja, ploscina je " + p);}
+				System.out.format("Trikotnik obstaja, ploscina je %.2f", p);}
 		
 		else {
 			if ((a + b == c) || (a + c == b) || (b + c == a))
