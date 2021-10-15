f51b2e3d26efab537608dae752d7b0c76327f1a7
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-15 20:58:27 +0200

    Add files via upload

---
+++ java/RAC/Funkcija2.java
@@ -0,0 +1,35 @@
+package Naloge;
+
+public class Funkcija2 {
+
+	public static void main(String[] args) {
+		
+		double y = 0;
+		int ascii = (int)'A';
+		
+		for (double x = -4; x <= 4; x = x + 0.5) {
+			if (x < -3) 
+				y = -1.5 * x - 6;
+			else if (x < -2)
+				y = -1.5;
+			else if (x < 0) 
+				y = 1.5 * x + 1.5;
+			else if (x < 1)
+				y = -1.5 * x + 1.5;
+			else if (x < 2)
+				y = x - 1;
+			else if (x < 3)
+				y = 1;
+			else
+				y = -x + 4;
+			
+			System.out.print((char)ascii);
+			System.out.format(" (%.1f", x);
+			System.out.format(", %.1f)\n", y);
+			
+			ascii += 1;
+		}
+
+	}
+
+}
