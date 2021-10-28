3603413b6964bd2f5e05e81b25b8d2dc565f9823
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-10-28 21:53:30 +0200

    Add files via upload

---
+++ java/RAC/Palindrom.java
@@ -0,0 +1,30 @@
+package Naloge;
+import java.util.Scanner;
+
+public class Palindrom {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Vnesi število: ");
+		String num = s.nextLine();
+		int length = num.length();
+		char a = ' ';
+		boolean jePalindrom = true;
+		s.close();
+		
+		//System.out.println(length);
+		for(int i = 0; i < length; i++) {
+			a = num.charAt(i);
+			if (a != (num.charAt((length - 1) - i)))
+				jePalindrom = false;
+		}
+		if (jePalindrom == true) {
+			System.out.println("Je palindrom");
+		}
+		else
+			System.out.println("Ni palindrom");
+	}
+
+}
