b10d8d1ca3f3bc992f1bfc6c007930894e32c0d3
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-18 20:49:05 +0100

    Create SahTrdnjava.java

---
+++ java/LAV/SahTrdnjava.java
@@ -0,0 +1,28 @@
+public class SahTrdnjava{
+
+     public static void main(String []args){
+        
+        int n = 8; //obseg polja
+        int ty = 5; //pozicija trdnajve y
+        int tx = 4; //pozicija trdnjave x
+        
+        for(int y = 1; y <= n; y++){ // navpicno polje
+            for(int x = 1; x <= n; x++){ //vodoravno polje
+                if((y == ty)&&(x == tx)) //pozicija trdnjave
+                    System.out.print('T');
+                        else if(y == ty) //vodoravni doseg
+                            System.out.print('-');
+                            else if(x == tx) //navpicni doseg
+                                System.out.print("|");
+                            else if((x % 2 == 0)&&(y % 2 != 0)) //prazna bela polja
+                                System.out.print('B');
+                                else if((x % 2 != 0) && (y % 2 == 0))
+                                    System.out.print('B');
+                                    else
+                                        System.out.print('C'); //prazna crna polja
+            }
+            System.out.println();
+        }
+        
+     }
+}
