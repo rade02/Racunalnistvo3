a379dcf3ad106f617d0ecedb31991bd0d73acf7b
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-18 21:03:44 +0100

    Update Osemindvajseta.java

--- java/LAV/VajeZanke/Osemindvajseta.java
+++ java/LAV/VajeZanke/Osemindvajseta.java
@@ -1,23 +1,22 @@
-package LAVvajeZanke;
+public class Osemindvajseta{
 
-public class Osemindvajseta {
-
-	public static void main(String[] args) {
-		
-		int a = 9999;
-		int vsota = 10;
-		
-		while(vsota >= 10) {
-		int e = a % 10;
-		int d = (a % 100) / 10;
-		int s = (a % 1000) / 100;
-		int t = (a % 10000) / 1000;
-		
-		vsota = e + d + s + t;
-		System.out.println(vsota);
-		a = vsota;
-		}
-		
-	}
-
+     public static void main(String []args){
+        
+        int n = 999999;
+        int vsota = 10;
+        
+        while(vsota >= 10){
+            int e = (int)(n % 10);
+            int d = (int)((n % 100) / 10);
+            int s = (int)((n % 1000) / 100);
+            int t = (int)((n % 10000) / 1000);
+            int dt = (int)((n % 100000) / 10000);
+            int st = (int)((n % 1000000) / 100000);
+            int m = (int)((n % 10000000) / 1000000);
+            
+            vsota = e + d + s + t + dt + st + m;
+            n = vsota;
+            System.out.println(vsota);
+        }
+     }
 }
