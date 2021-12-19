1d161249859dd8196aa1ea4b158be3a971e0865f
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-19 15:50:36 +0100

    Update DesniTrikotnikIzZvezdic.java

--- java/LAV/DesniTrikotnikIzZvezdic.java
+++ java/LAV/DesniTrikotnikIzZvezdic.java
@@ -6,7 +6,7 @@ public class DesniTrikotnikIzZvezdic{
         
         for(int i = 0; i < n; i++){
             for(int j = 0; j < n; j++){
-                if(i + j >= n - 1)
+                if(i + j > n)
                     System.out.print('*');
                 else
                     System.out.print('-');
