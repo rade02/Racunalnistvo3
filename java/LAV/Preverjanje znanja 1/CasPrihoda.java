1f41449e1838c1d1145e5a2db5e8704b101c9896
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-19 14:13:00 +0100

    Update CasPrihoda.java

--- java/LAV/Preverjanje znanja 1/CasPrihoda.java
+++ java/LAV/Preverjanje znanja 1/CasPrihoda.java
@@ -1,26 +1,21 @@
-package preverjanje1;
 import java.util.Scanner;
+public class CasPrihoda{
 
-public class CasPrihoda {
-
-	public static void main(String[] args) {
-		
-	    Scanner s = new Scanner(System.in);
-	    System.out.println("Vnesi uro odhoda: ");
-	    int ura = s.nextInt();
-	    System.out.println("Vnesi minuto odhoda: ");
-	    int min = s.nextInt();
-	    System.out.println("Vnesi trajanje: ");
-	    int trajanje = s.nextInt();
-	    s.close();
-	    
-	    double minute = min + trajanje;
-	    double ure = ura + minute%60;
-	    minute %= 60;
-	    
-	    System.out.println("Prispe ob: " + ((ure < 10) ? ("0" + (int)ure):(int)ure) + ":" + ((min < 10) ? ("0" + (int)minute):(int)minute));
-	    
-	    }
-		
-
+     public static void main(String []args){
+        
+        Scanner s = new Scanner(System.in);
+        System.out.println("Vnesi uro odhoda: ");
+        int ura = s.nextInt();
+        System.out.println("Vnesi minuto odhoda: ");
+        int min = s.nextInt();
+        System.out.println("Vnesi trajanje v min: ");
+        int trajanje = s.nextInt();
+        s.close();
+        
+        int skMin = min + trajanje; //najprej sestejemo minute
+        int skUre = ura + skMin / 60; //uri pristejemo polne ure minut
+        skMin = skMin % 60; //minutam odstranimo ure
+        
+        System.out.print("Cas prihoda: " + ((skUre < 10)?("0" + skUre):skUre) + ":" + ((skMin < 10)?("0" + skMin):skMin));
+     }
 }
