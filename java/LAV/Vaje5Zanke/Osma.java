bdef1012edf3411c76c2435ba0dbae9f5dc7ea36
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-19 19:50:33 +0100

    Create Osma.java

---
+++ java/LAV/Vaje5Zanke/Osma.java
@@ -0,0 +1,15 @@
+public class Osma {
+
+	public static void main(String[] args) {
+		
+		int i = 10;
+		
+		while(i >= -10){
+		    if(i % 2 != 0)
+		        System.out.println(i);
+		    i--;
+		}
+		
+	}
+
+}
