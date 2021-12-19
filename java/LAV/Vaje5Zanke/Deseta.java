86e166facb31c51d177a76bbe65f43eaabd4ae5a
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-19 19:52:26 +0100

    Create Deseta.java

---
+++ java/LAV/Vaje5Zanke/Deseta.java
@@ -0,0 +1,16 @@
+public class Deseta {
+
+	public static void main(String[] args) {
+		
+		int i = 1, vsota = 0;
+		
+		while(i <= 100){
+		    if(i % 2 != 0)
+		        vsota += i;
+		    i++;
+		}
+		System.out.println(vsota);
+		
+	}
+
+}
