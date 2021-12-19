17a88b8bb779676686f5a7a01f532daab9765da1
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-19 15:29:48 +0100

    Update Jelka.java

--- java/LAV/Jelka.java
+++ java/LAV/Jelka.java
@@ -4,20 +4,17 @@ public class Jelka{
         
         int n = 10;
         
-        
-        for(int i = 0; i < n; i++){ //za visino smreke
+        for(int i = 0; i < n; i++){ //za visino drevesa
             for(int j = 0; j < n - i; j++){ //za prazne prostore pred *, do sredine
                 System.out.print('-');
             }
             for(int k = 0; k < 2*i + 1; k++){ //za *, stevilo njih je 2*i+1
                 System.out.print('*');
-            }    
-            //crte na desni strani smreke
-            for(int l = 0; l < n - i; l++){
+            }
+            for(int l = 0; l < n - i; l++){ //crte na desni strani drevesa
                 System.out.print('-');
             }
-            //v novo vrsto
-            System.out.println();
+            System.out.println(); //skok v novo vrsto
         }
         
      }
