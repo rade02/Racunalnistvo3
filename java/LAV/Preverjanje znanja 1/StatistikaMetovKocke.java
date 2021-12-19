9ba02e54d4c8d20903a695a407b4d9ef6f94ce7a
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-19 14:19:29 +0100

    Update StatistikaMetovKocke.java

--- java/LAV/Preverjanje znanja 1/StatistikaMetovKocke.java
+++ java/LAV/Preverjanje znanja 1/StatistikaMetovKocke.java
@@ -1,24 +1,20 @@
-package preverjanje1;
+public class StatistikaMetovKocke{
 
-public class StatistikaMetovKocke {
-
-	public static void main(String[] args) {
-		
-		int ena = 0, dva = 0, tri = 0, stiri = 0, pet = 0, sest = 0;
-		
-		for(int i = 0; i < 1000; i++) {
-			int a = (int)(Math.random()*6 + 1);
-			switch(a) {
-			case 1: ena++; break;
-			case 2: dva++; break;
-			case 3: tri++; break;
-			case 4: stiri++; break;
-			case 5: pet++; break;
-			case 6: sest++; break;
-			}
-		}
-		System.out.println("Enkra: " + ena + ", dvojka: " + dva + ", trojka: " + tri + ", stirica: " + stiri + ", petka: " + pet + ", sestica: " + sest);
-
-	}
-
+     public static void main(String []args){
+        
+        int enka = 0, dvojka = 0, trojka = 0, stirica = 0, petka = 0, sestica = 0;
+        
+        for(int i = 0; i < 1000; i++){
+            int n = (int)(Math.random()*6+1);
+            switch(n){
+                case 1: enka++;break;
+                case 2: dvojka++;break;
+                case 3: trojka++; break;
+                case 4: stirica++;break;
+                case 5: petka++; break;
+                case 6: sestica++; break;
+            }
+        }
+        System.out.println(enka  + " " + dvojka + " " + trojka + " " + stirica + " " + petka + " " + sestica + " | " + (enka+dvojka+trojka+stirica+petka+sestica));
+     }
 }
