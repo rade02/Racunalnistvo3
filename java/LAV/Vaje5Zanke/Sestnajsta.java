98d48a17b508e23c60d832cc3b058b16468eb314
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-20 17:42:29 +0100

    Create Sestnajsta.java

---
+++ java/LAV/Vaje5Zanke/Sestnajsta.java
@@ -0,0 +1,22 @@
+public class Sestnajsta{
+
+     public static void main(String []args){
+        
+        int n = 129;
+        
+        for(int i = 2; i <= n; i++){
+            boolean jePrastevilo = true;
+            for(int j = 2; j <= Math.sqrt(i); j++){ //ali je i prastevilo
+                if(i % j == 0)
+                    jePrastevilo = false;
+            }
+            if((n % i == 0) && (jePrastevilo == true)){ //ce je i prastevilo in deli n
+                System.out.print(i + " ");
+                n /= i; //n postane n/i
+                i = 2; //potrebno je ponastaviti i, da zanka zacne od zacetka (lahko imamo vec enakih prafaktorjev)
+            }
+            
+        }
+        System.out.print(1);
+     }
+}
