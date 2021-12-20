a00712131f714eb1e4b30a4ab770395e54ca4aba
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-20 18:05:38 +0100

    Create Osemnajsta.java

---
+++ java/LAV/Vaje5Zanke/Osemnajsta.java
@@ -0,0 +1,23 @@
+public class Osemnajsta{
+
+     public static void main(String []args){
+        
+        int stevec = 1, n = 100, st = 2; //n-to prastevilo, st je prvo prastevilo
+        
+        while(stevec <= n){ //pregledamo vsa prastevila do n-tega
+            boolean jePrastevilo = true;
+            for(int i = 2; i <= Math.sqrt(st); i++){ //ali je st prastevilo
+                if(st % i == 0)
+                    jePrastevilo = false;
+            }
+            
+            if(jePrastevilo == true){ //ce je prastevilo povecamo stevec
+                if(stevec == n)
+                    System.out.print(st);
+                stevec++;
+            }
+            st++; //da z naslednjo iteracijo preverjamo novo stevilo
+        }
+        
+     }
+}
