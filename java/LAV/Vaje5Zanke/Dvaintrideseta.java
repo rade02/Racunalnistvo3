a89c3590e406ae4ecd997bd82d68de2ddbbbbe4f
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-21 17:54:04 +0100

    Update Dvaintrideseta.java

--- java/LAV/Vaje5Zanke/Dvaintrideseta.java
+++ java/LAV/Vaje5Zanke/Dvaintrideseta.java
@@ -4,19 +4,19 @@ public class Dvaintrideseta {
 
 	public static void main(String[] args) {
 		
-		  Scanner s = new Scanner(System.in);
-		  System.out.println("Vnesi prvi clen zaporedja: ");
-      int n = s.nextInt();
-      s.close();
+		Scanner s = new Scanner(System.in);
+		System.out.println("Vnesi prvi clen zaporedja: ");
+      		int n = s.nextInt();
+      		s.close();
         
-        System.out.print(n + " ");
-        while(n != 1){
-            if(n % 2 == 0)
-                n = n / 2;
-            else
-                n = 3*n + 1;
-            System.out.print(n + " ");
-        }
+        	System.out.print(n + " ");
+        	while(n != 1){
+            		if(n % 2 == 0)
+                		n = n / 2;
+            		else
+                		n = 3*n + 1;
+            		System.out.print(n + " ");
+        	}
 
 	}
 
