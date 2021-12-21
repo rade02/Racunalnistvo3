042e50979a417a945a8c17fbbdd23eedb9d87204
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-21 17:39:57 +0100

    Update Sestindvajseta.java

--- java/LAV/Vaje5Zanke/Sestindvajseta.java
+++ java/LAV/Vaje5Zanke/Sestindvajseta.java
@@ -1,25 +1,20 @@
-package LAVvajeZanke;
-
 public class Sestindvajseta {
 
 	public static void main(String[] args) {
 		
-		int min = 1; 
-		int max = 10000;
-		int vsota = 0;
+		int min = 10, max = 9000;
 		
-		for(int i = min; i <= max; i++) { //interval
-			
-			for(int j = 1; j < i; j++) {
-				if(i % j == 0)
-					vsota += j;
-			}
-			if(vsota == i) {
-				System.out.println(i);
-			}
-			
+		for(int i = min; i <= max; i++){
+		    int vsota = 0;
+		    for(int j = 1; j <= i/2; j++){
+		        if(i % j == 0){
+		            vsota += j;
+		        }
+		    }
+		    if(vsota == i)
+		        System.out.println(i);
 		}
-
+		
 	}
 
 }
