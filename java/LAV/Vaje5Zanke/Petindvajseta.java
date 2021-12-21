2c97d7c60c6eed5caf56715f2cbba397c1bacae9
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2021-12-21 21:02:56 +0100

    Update Petindvajseta.java

--- java/LAV/Vaje5Zanke/Petindvajseta.java
+++ java/LAV/Vaje5Zanke/Petindvajseta.java
@@ -1,18 +1,15 @@
-package LAVvajeZanke;
+public class Petindvajseta{
 
-public class Petindvajseta {
-
-	public static void main(String[] args) {
-		
-		int n = 1003;
-
-		int e = n % 10;
-		int d = (n % 100) - e;
-		int s = (n % 1000) / 100;
-		int t = (n % 10000) / 1000;
-		
-		int vsota = e + d + s + t;
-		System.out.println(vsota);
-	}
-
+     public static void main(String []args){
+        
+        int n = 12305, vsota = 0;
+        
+        while(n > 0){
+            int a = n % 10;
+            vsota += a;
+            n /= 10;
+        }
+        System.out.println(vsota);
+        
+     }
 }
