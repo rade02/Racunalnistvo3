0be0b523556183db8a73696f98558a25ba4c28e7
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-01-09 10:24:35 +0100

    Create SelectionSort.java

---
+++ java/RAC/Tabele/SelectionSort.java
@@ -0,0 +1,41 @@
+public class SelectionSort{
+
+     public static void main(String []args){
+        
+        int tab[] = {1, 5, 3, 9, 8};
+        int prviNeurejen = 0; //indeks prvega neurejenega stevila
+        
+        //izpis zacetne tabele
+        System.out.print(" | ");
+        for(int j = 0; j < tab.length; j++)
+            System.out.print(tab[j] + " | ");
+        System.out.println("\n");
+        
+        //dokler ne pridemo do konca tabele...
+        while(prviNeurejen < tab.length - 1){
+            
+            int min = prviNeurejen; //indeks najmanjsega stevila
+            int stPrvoNeurejeno = tab[prviNeurejen]; //prvo neurejeno stevilo
+            
+            //gremo cez neurejen del in najdemo indeks najmanjsega stevila
+            for(int i = prviNeurejen; i < tab.length; i++){
+                if(tab[i] < tab[min])
+                    min = i;
+            }
+            
+            //zamenjamo prvo neurejeno stevilo s stevilom na indeksu min (najmanjsim stevilom v neurejenem delu)
+            int tmp = tab[prviNeurejen];
+            tab[prviNeurejen] = tab[min];
+            tab[min] = tmp;
+            
+            //povecujemo urejen del
+            prviNeurejen++;
+        }
+        
+        //izpis urejene tabele
+        System.out.print(" | ");
+        for(int j = 0; j < tab.length; j++)
+            System.out.print(tab[j] + " | ");
+        
+     }
+}
