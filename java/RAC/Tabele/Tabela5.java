7277527bab90a22b5c53be0d3290a49a069b37c7
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-01-20 15:59:32 +0100

    Create Tabela5.java

---
+++ java/RAC/Tabele/Tabela5.java
@@ -0,0 +1,21 @@
+public class Tabela5 {
+    public static void main(String[] args){
+        
+        int n = 5; //dolzina kvadratne tabele
+        int tab[][] = new int[n][n];
+
+        for(int i = 0; i < tab.length; i++){
+            for(int j = 0; j < tab.length; j++){ //tukaj lahko uporabimo tab.length brez [0], ker je tabela kvadratna
+                if((i == j)||(i + j == tab.length - 1))
+                    tab[i][j] = 1;
+                else
+                tab[i][j] = 0;
+            }
+        }
+        for(int i = 0; i < tab.length; i++){
+            for(int j = 0; j < tab.length; j++)
+                System.out.print(tab[i][j] + " ");
+            System.out.println();
+        }
+    }
+}
