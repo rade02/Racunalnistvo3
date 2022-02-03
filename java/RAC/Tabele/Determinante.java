40caab7420074a485773071794f0521ef2098edc
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-02-03 18:03:35 +0100

    Update Determinante.java

--- java/RAC/Tabele/Determinante.java
+++ java/RAC/Tabele/Determinante.java
@@ -1,7 +1,7 @@
 public class Determinante {
     public static void main(String[] args){
         
-        //determinanta matrike A
+        ///*determinanta matrike A
         int[][] A = {{2, -3}, {1, -1}};
         int detA = A[0][0] * A[1][1] - A[0][1] * A[1][0];
         System.out.println("det(A) = " + detA);
@@ -24,7 +24,7 @@ public class Determinante {
         int detB = B[0][0]*B[1][1] - B[0][1]*B[1][0];
         System.out.println("det(B) = " + detB);
 
-        //determinanta, ce matriko mnozimo s skalarjem
+        //determinanta, ce matriko A mnozimo s skalarjem
         int[][] C = new int[A.length][A[0].length];
         for(int i = 0; i < C.length; i++){
             for(int j = 0; j < C[0].length; j++){
@@ -35,5 +35,13 @@ public class Determinante {
         }
         int detC = C[0][0]*C[1][1] - C[0][1]*C[1][0];
         System.out.println("det(C) = " + detC);
+        //*/
+
+        /*Sarrusovo pravilo za determinanto 3. reda
+        int[][] D = {{2, -1, 1}, {0, -2, 3}, {1, -2, 2}};
+
+        int detD = D[0][0]*D[1][1]*D[2][2] + D[0][1]*D[1][2]*D[2][0] + D[0][2]*D[1][0]*D[2][1] - D[0][2]*D[1][1]*D[2][0] - D[0][0]*D[1][2]*D[2][1] - D[0][1]*D[1][0]*D[2][0];
+        System.out.println("det(D) = " + detD);
+        */
     }
 }
