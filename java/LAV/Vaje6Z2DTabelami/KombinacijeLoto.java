5d826c6dcd80c203c456790aa0643aa9dea506fb
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-02-05 14:18:51 +0100

    Update KombinacijeLoto.java

--- java/LAV/Vaje6Z2DTabelami/KombinacijeLoto.java
+++ java/LAV/Vaje6Z2DTabelami/KombinacijeLoto.java
@@ -1,28 +1,54 @@
 import java.util.Scanner;
 public class KombinacijeLoto {
-  
     public static void main(String[] args){
-      
         Scanner s = new Scanner(System.in);
+
+        int stevilaDo = 10;
+        int stevil = 7;
+        int[] izzrebana = new int[stevil];
+
+        //vnos stevil izzrebane kombinacije
+        int stevec = 1;
+        do{
+        System.out.println("Vnesi " + stevec + ". stevilo izzrebane kombinacije: ");
+        izzrebana[stevec - 1] = s.nextInt();
+        stevec++;
+        } while(stevec <= 7);
+        
+        //izpis izzrebane kombinacije
+        for(int i = 0; i < izzrebana.length; i++)
+            System.out.print(izzrebana[i] + " ");
+        System.out.println();
+
         System.out.println("Koliko kombinacij? ");
         int n = s.nextInt();
         s.close();
-        int stevil = 7;
-        int[][] tab = new int[n][stevil];
-        int stevilaDo = 10;
+        System.out.println();
         
+        int[][] tab = new int[n][stevil];
+        
+
         for(int i = 0; i < tab.length; i++){
+
             int st[] = new int[stevilaDo + 1]; //da se stevila ne ponovijo, njihovo pojavnost zapisemo v tabelo
+            int zadetkov = 0;
+
             for(int j = 0; j < tab[0].length; j++){
+
                 tab[i][j] = (int)(Math.random()*(stevilaDo + 1));
-                if(st[tab[i][j]] == 0){ //ce stevila se ni v kombinaciji, ga izpisemo
+
+                if(st[tab[i][j]] == 0){ //ce se nakljucno stevilo se ni pojavilo v kombinaciji
                     System.out.print(((tab[i][j] < 10) ? (" " + tab[i][j]):(tab[i][j])) + " ");
                     st[tab[i][j]]++;
+                    //ce je vrednost v tabeli enaka izzrebani vrednosti
+                    if(tab[i][j] == izzrebana[j]){
+                        zadetkov++;
+                    }
                 }
-                else //ce je stevilo ze v kombinaciji, ga preskocimo in zmanjsamo stevec stevil v kombinaciji
+                else
                     j--;
             }
-            System.out.println();
+            System.out.println(" Zadeli smo " + zadetkov + " stevil v kombinaciji.");
         }
     }
 }
