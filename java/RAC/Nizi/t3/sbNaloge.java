8f8abe08bc70559f716389a480c7788dd22ea1de
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-03-20 10:16:48 +0100

    Add files via upload

--- java/RAC/Nizi/t3/sbNaloge.java
+++ java/RAC/Nizi/t3/sbNaloge.java
@@ -16,6 +16,41 @@ public class sbNaloge {
             if(Character.isWhitespace(sb.charAt(i)))
                 stPresledkov++;
         }
-        System.out.println(stPresledkov);
+        System.out.println("vsebuje: " + stPresledkov + " presledkov");
+
+        //koliko je saomglasnikov
+        char[] samogl = {'a', 'e', 'i', 'o', 'u'};
+        int stSamogl = 0;
+        for(int i = 0; i < sb.length(); i++){
+            for(int j = 0; j < samogl.length; j++){
+                if(sb.charAt(i) == samogl[j]){
+                    stSamogl++;
+                    break;
+                }
+            }
+        }
+        System.out.println("vsebuje: " + stSamogl + " samoglasnikov");
+
+        //koliko povedi je v nizu
+        int stPovedi = 0;
+        for(int i = 0; i < sb.length(); i++){
+            if(sb.charAt(i) == '.'){
+                stPovedi++;
+            }
+        }
+        System.out.println("skupaj je " + stPovedi + " povedi");
+
+        //prvi niz "la" v nizu
+        String niz2 = "la";
+        int indeks = 0;
+        for(int i = 0; i < sb.length(); i++){
+            indeks = sb.indexOf(niz2); //ne dela metoda .toLowerCase() za StringBuffer
+        }
+        System.out.println("prvi niz " + niz2 + " se nahaja na indeksu " + indeks); //upošteva tudi razliko med velikimi/malimi črkami
+
+        //kje je zadnja pika v nizu
+        int i2 = sb.lastIndexOf(".");
+        System.out.println(i2);
+
     }
 }
