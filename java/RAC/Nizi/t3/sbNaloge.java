b6c03e68c3144d99c30937d09f53356e15272fdd
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-03-22 19:57:32 +0100

    Update sbNaloge.java

--- java/RAC/Nizi/t3/sbNaloge.java
+++ java/RAC/Nizi/t3/sbNaloge.java
@@ -1,4 +1,4 @@
-package t3;
+import java.util.Arrays;
 
 public class sbNaloge {
     public static void main(String[] args){
@@ -7,9 +7,39 @@ public class sbNaloge {
 
         StringBuffer sb = new StringBuffer(niz);
 
-        //katera beseda se najveckrat ponovi??
-        //..
-    
+        //katera beseda se najveckrat ponovi
+        for(int i = 0; i < sb.length(); i++){   //naredimo StringBuffer brez locil
+            if(!(Character.isLetter(sb.charAt(i)) || (Character.isWhitespace(sb.charAt(i)) || (Character.isDigit(sb.charAt(i)))))){
+                sb.deleteCharAt(i);
+            }
+        }
+        String n = new String(sb.toString());   //naredimo String za .split()
+        String[] tab = n.split(" ");
+        Arrays.sort(tab);   //sortiramo tabelo
+
+        int stevec = 0, najvecEnakih = 0, enakih = 0;
+        String najveckratPonovljena = "";
+        //System.out.println(Arrays.toString(tab));
+
+        while(stevec < tab.length - 1){ //gremo cez celo tabelo
+            enakih = 1;
+            int s = stevec;
+            
+            while(true){   //dokler so enaki elementi, iscemo, koliko je enakih
+                if(!(tab[s].equals(tab[s + 1]))){
+                    break;
+                }
+                enakih++;
+                s++;
+            }
+            if(enakih > najvecEnakih){
+                najvecEnakih = enakih;
+                najveckratPonovljena = tab[stevec];
+            }
+            stevec += enakih;
+        }
+        System.out.println("Najveckrat se ponovi beseda: " + najveckratPonovljena + ", in sicer " + najvecEnakih + "-krat.");
+        
         //koliko je presledkov
         int stPresledkov = 0;
         for(int i = 0; i < sb.length(); i++){
@@ -18,7 +48,7 @@ public class sbNaloge {
         }
         System.out.println("vsebuje: " + stPresledkov + " presledkov");
 
-        //koliko je saomglasnikov
+        //koliko je samoglasnikov
         char[] samogl = {'a', 'e', 'i', 'o', 'u'};
         int stSamogl = 0;
         for(int i = 0; i < sb.length(); i++){
@@ -51,6 +81,6 @@ public class sbNaloge {
         //kje je zadnja pika v nizu
         int i2 = sb.lastIndexOf(".");
         System.out.println(i2);
-
+        
     }
 }
