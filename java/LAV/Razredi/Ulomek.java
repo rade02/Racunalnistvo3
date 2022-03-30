75ff4d92f98af4df4bcdac9306377a67c914cf18
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-03-30 12:00:38 +0200

    Create Ulomek.java

---
+++ java/LAV/Razredi/Ulomek.java
@@ -0,0 +1,16 @@
+package razredi;
+
+public class Ulomek {
+
+	public static void main(String[] args) {
+		
+		int stevec = 12;
+		int imenovalec = 3;
+		
+		Ulomki ulomek = new Ulomki(stevec, imenovalec);
+		ulomek.pokrajsaj(stevec, imenovalec);
+		
+		
+	}
+
+}
