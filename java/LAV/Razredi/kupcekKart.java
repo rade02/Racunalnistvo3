9cb73b1da8feb319c0677c72720d0cec0860c11e
Erik Radovicevic <79723555+rade02@users.noreply.github.com>
2022-05-04 11:58:53 +0200

    Create kupcekKart.java

---
+++ java/LAV/Razredi/kupcekKart.java
@@ -0,0 +1,32 @@
+class kupcekKart{
+    private int stevilo;
+    private karta[] kupcek = new karta[52];
+    priavte String[] barve ) {"kriz", "pik", "kara", "srce"};
+
+    public kupcekKart(){
+        this.stevilo = 0;
+        for(int i = 0; i < 4; i++){ //napolnim kupcek
+            for(int j = 0; j < 13; j++){
+                this.dodaj(new karta(barve[i], j+2));
+            }
+        }
+    }
+    public kupcekKart(){
+        this();     //klic drugega konstruktorja - mora biti na zacetku
+        System.out.println("klic drugega konstruktorja");
+    }
+
+    public void dodaj(karta nova){
+        if(stevilo<52){
+            kupcek[stevilo] = nova;
+            stevilo++;
+        }
+    }
+
+}
+
+public class vajaKarte{
+    public static void main(String[] args){
+        karta druga = new karta("pik", 8);
+    }
+}
